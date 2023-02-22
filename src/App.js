06e7088555254f9b333ad9b4d5a947bac601a20f
aao056 <a_ogiolan@Yahoo.com>
2023-02-22 15:51:20 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+import illustration from "./images/illustration-hero.svg"
+import music_svg from "./images/icon-music.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className='container'>
+        <div className='hero'>
+          <img src={illustration} alt=''/>
+        </div>
+      <div className='text-content'>
+        <h2 className='title'>
+            Order Summary
+        </h2>
+        <p className='subtitle'>
+          You can now listen to millions of songs, audiobooks, 
+          and podcasts on any device you like anywhere you like!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className='plan-box'>
+          <div className='plan-box-left'> 
+            <img src={music_svg} alt=''/>
+            <div>
+              <h5>Annual Plan</h5>
+              <p>$59.99/year</p>
+            </div>
+          </div>
+            <a href='#'>Change</a>
+        </div>
+        <a href='#' className='proceed-btn'>Proceed to Payment </a>
+        <a href='#' className='cancel-btn'>Cancel Order</a>
+      </div>
+      </div>
+    </>
   );
 }
 
